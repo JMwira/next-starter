0a6da64fa069c5b8ff7de4cee0dc8a314d06bfd1
JMwira <jaymwira@gmail.com>
2023-03-07 03:01:49 +0530

    key-feature

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -3,13 +3,15 @@ import { Box, Text, Heading } from 'theme-ui';
 
 export default function SectionHeader({ title, slogan, isWhite }) {
   return (
-    <Box sx={{ variant: 'sectionHeader' }}>
+    <Box
+      sx={{variant:"sectionHeader"}}
+    >
       <Text
         as="p"
         sx={{
-          variant: 'sectionHeader.subTitle',
-          color: isWhite ? 'white' : 'primary',
-          opacity: isWhite ? 0.7 : 1,
+          variant:"sectionHeader.subTitle",
+          color:isWhite?'white':'primary',
+          opacity:isWhite?.7:1
         }}
       >
         {slogan}
@@ -17,8 +19,8 @@ export default function SectionHeader({ title, slogan, isWhite }) {
       <Heading
         as="h2"
         sx={{
-          variant: 'sectionHeader.title',
-          color: isWhite ? 'white' : 'heading',
+          variant:"sectionHeader.title",
+          color:isWhite?'white':'heading'
         }}
       >
         {title}
